17cfc268eac608294accb5643824e7cdbab763e1
walaasalah4 <121608451+walaasalah4@users.noreply.github.com>
2022-12-29 12:31:43 +0200

    Add files via upload

---
+++ PRO 1 prime number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PRO_1_prime_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double  n;
+            Console.Write("Enter the number");
+            n = double.Parse(Console.ReadLine());
+            for (double i = 2; i <= n - 1; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine("the number isn't prime");
+                }
+
+                if (n % i != 0)
+                {
+                    Console.WriteLine("the number is prime");
+
+
+
+                }
+            }
+        }
+    }
+}
